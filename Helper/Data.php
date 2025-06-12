d178473689f5731815c248dd245f00c1aea86489
Bernard Bachmann <bb@cytracon.com>
2025-06-12 13:07:19 +0200

    120625

--- Helper/Data.php
+++ Helper/Data.php
@@ -8,20 +8,16 @@ class Data extends AbstractHelper
 {
     const XML_PATH_CSS = 'cytracon_stylesheet/general/custom_css';
 
-    public function getCustomCss()
+    public function getCustomCss(): string
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_CSS, ScopeInterface::SCOPE_STORE);
+        return (string) $this->scopeConfig->getValue(self::XML_PATH_CSS, ScopeInterface::SCOPE_STORE);
     }
 
-    public function minifyCss($css)
+    public function minifyCss(string $css): string
     {
         // Simples Regex-basiertes Minify
         $css = preg_replace('!/\*.*?\*/!s', '', $css);      // Kommentare entfernen
-        $css = preg_replace('/
-\s*
-/', "\n", $css);       // Mehrere Leerzeilen auf eine reduzieren
-        $css = preg_replace('/[\n\r \t]/', '', $css);       // Whitespace entfernen
-        $css = preg_replace('/ +/', ' ', $css);
+        $css = preg_replace('/\s+/', ' ', $css);            // Mehrere Whitespaces auf einen reduzieren
         $css = preg_replace('/ ?([,:;{}]) ?/', '$1', $css); // Spaces um : ; , { } entfernen
         return trim($css);
     }
